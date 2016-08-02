6e3d1b898eba26df1e31361360e94d82d5d43a7f
Jennifer Chang <jennichang02@gmail.com>
2016-08-02 16:56:52 -0400

    with Assignment 2 (8/2)

--- src/com/theironyard/CustomMath.java
+++ src/com/theironyard/CustomMath.java
@@ -15,4 +15,25 @@ public class CustomMath {
     }
 
 
+    // Quadratic formula
+    public static double getQuadDiscr(double a, double b, double c) {
+        double quadDiscr;
+        quadDiscr = (Math.sqrt((b * b) - (4 * a * c)));
+        return quadDiscr;
+    }
+
+    public static double getQuadraticRoot1(double a, double b, double c) {
+        double root1;
+        root1 = (-b + Math.sqrt(Math.pow(b,2) - 4*a*c)) / (2*a);
+        return root1;
+    }
+
+
+    public static double getQuadraticRoot2(double a, double b, double c) {
+        double root2;
+        root2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        return root2;
+    }
+
+
 }
--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,25 +3,34 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Stats s = new Stats();
         s.setGender("Male");
         s.setWeight(200);
         s.setHeight(600);
 
+        Profile p = new Profile();
+        p.setName("Jennifer");
+        p.setAge(900);
+        p.setSingle(false);
+
         System.out.println("Area of circle equals " + CustomMath.getArea(5));
-        System.out.println("Hypotenuse of triangle equals " + CustomMath.getHypotenuse(10,6));
+        System.out.println("Hypotenuse of triangle equals " + CustomMath.getHypotenuse(10, 6));
 
 
+         /* if (CustomMath.getQuadDiscr(3, 4, 4) > 0) {
+            System.out.println("Root1 of Quadratic formula is " + CustomMath.getQuadraticRoot1(3, 4, 4) + " and Root2" +
+                    " of Quadratic formula is " + CustomMath.getQuadraticRoot2(3, 4, 4));
+        } else if (CustomMath.getQuadDiscr(3, 4, 4) == 0) {
+            System.out.println("Roots of Quadratic formula are " + CustomMath.getQuadraticRoot1(3, 4, 4));
+        } else System.out.println("Equation has imaginary roots"); */
 
-
-
-
-
-
-
-
-
+        if (CustomMath.getQuadDiscr(54, 64, 7) < 0) {
+            System.out.println("The equation has imaginary roots");
+        } else
+            System.out.println("Root1 equals " + CustomMath.getQuadraticRoot1(54, 64, 7) + " and Root2 equals " +
+                    CustomMath.getQuadraticRoot2(54, 64, 7));
 
     }
+
 }
